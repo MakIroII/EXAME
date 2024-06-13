d2207db1b18f6357d4c7e19907921708cf89ea90
Елена Плюхина <E_Plyukhina@tervolina.ru>
2024-06-13 09:03:53 +0300

    A

---
+++ solar_main.py
@@ -0,0 +1,209 @@
+import os
+from tkinter.filedialog import *
+
+from solar_objects import *
+
+perform_execution = False
+"""Флаг цикличности выполнения расчёта"""
+
+orbit_perform = False
+"""Флаг цикличности выполнения обновления изображения орбит """
+
+physical_time = 0, 0
+"""Физическое время от начала расчёта.
+Тип: float"""
+
+displayed_time = 0.0
+"""Отображаемое на экране время.
+Тип: переменная tkinter"""
+
+time_step = 0
+"""Шаг по времени при моделировании.
+Тип: float"""
+
+r = 0
+
+time_speed = 0
+
+space_objects = []
+"""Список космических объектов."""
+
+
+def create_orbit():
+    global orbit_perform
+    orbit_perform = True
+    screen.trigger_button(4, "Delete Orbit", delete_orbit)
+    screen.create_orbit_image(space_objects)
+    screen.tag_lower('circle')
+
+
+def delete_orbit():
+    screen.create_orbit_image([])
+    global orbit_perform
+    orbit_perform = False
+    screen.trigger_button(4, "Create Orbit", create_orbit)
+
+
+def execution():
+    """Функция исполнения -- выполняется циклически, вызывая обработку всех небесных тел,
+    а также обновляя их положение на экране.
+    Цикличность выполнения зависит от значения глобальной переменной perform_execution.
+    При perform_execution == True функция запрашивает вызов самой себя по таймеру через 1-100 миллисекунд.
+    """
+    global physical_time
+    global displayed_time
+
+    try:
+        p.move(space_objects, time_step.get())
+        sp.move(space_objects, time_step.get(), r)
+        screen.update_object_position(space_objects, orbit_perform)
+        physical_time += time_step.get()
+        displayed_time.set("%.1f" % physical_time + " seconds gone")
+
+        if perform_execution:
+            screen.after(time_speed.get(), execution)
+    except NameError:
+        physical_time += time_step.get()
+        displayed_time.set("%.1f" % physical_time + " seconds gone")
+
+        if perform_execution:
+            screen.after(time_speed.get(), execution)
+        pass
+    except TclError:
+        pass
+
+
+def start_execution():
+    """Обработчик события нажатия на кнопку Start.
+    Запускает циклическое исполнение функции execution.
+    """
+    global perform_execution
+    perform_execution = True
+    screen.trigger_button(1, "Pause", stop_execution)
+    execution()
+    print('Started execution...')
+
+
+def stop_execution():
+    """Обработчик события нажатия на кнопку Start.
+    Останавливает циклическое исполнение функции execution.
+    """
+    global perform_execution
+    perform_execution = False
+    screen.trigger_button(1, "Start", start_execution)
+    print('Paused execution.')
+
+
+def open_file_dialog():
+    """Открывает диалоговое окно выбора имени файла и вызывает
+    функцию считывания параметров системы небесных тел из данного файла.
+    Считанные объекты сохраняются в глобальный список space_objects
+    """
+    global space_objects
+    global perform_execution
+    global st
+    global sp
+    global p
+    global r
+    perform_execution = False
+    for obj in space_objects:
+        screen.delete(obj[6])
+        # удаление старых изображений планет
+    space_objects = []
+    in_filename = askopenfilename(filetypes=(("Text file", ".txt"),))
+    filename = os.path.splitext(os.path.basename(in_filename))[0]
+
+    screen.update_system_name(filename)
+    st = Star(in_filename)
+    p = Planet(in_filename)
+    sp = Sputnik(in_filename)
+    try:
+        p.read_parameters()
+        st.read_parameters()
+        sp.read_parameters()
+        sp.append(space_objects)
+        p.append(space_objects)
+        st.append(space_objects)
+        r = sp.radius(space_objects)
+        f=[]
+        for obj in space_objects:
+            for  body in space_objects:
+                if obj[0]== "Planet" and body[0] == "Star" and obj[8]==body[8]:
+                    f.append(((body[4]-obj[4])**2+(body[5]-obj[5])**2)**0.5)
+        max_distance=max(f)
+        print(max_distance)
+        screen.change_distance(max_distance)
+        p.move(space_objects, 0)
+        sp.move(space_objects, 0, r)
+        for obj in space_objects:
+            screen.create_image(obj)
+    except (FileNotFoundError, UnicodeDecodeError, IndexError,ValueError):
+        pass
+
+
+def save_file_dialog():
+    """Открывает диалоговое окно выбора имени файла и вызывает
+    функцию считывания параметров системы небесных тел из данного файла.
+    Считанные объекты сохраняются в глобальный список space_objects
+    """
+    out_filename = asksaveasfilename(filetypes=(("Text file", ".txt"),))
+    try:
+        p.write_data_to_file(out_filename, 'w')
+        st.write_data_to_file(out_filename, 'a')
+        sp.write_data_to_file(out_filename, 'a')
+    except NameError:
+        pass
+
+
+def static_save_dialog():
+    out_filename = asksaveasfilename(filetypes=(("Text file", ".txt"),))
+    try:
+        st.write_static_to_file(out_filename, 'w')
+        p.write_static_to_file(out_filename, 'a')
+        sp.write_static_to_file(out_filename, 'a')
+    except NameError:
+        pass
+
+
+def main():
+    """Главная функция главного модуля.
+    Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
+    """
+    global physical_time
+    global displayed_time
+    global time_step
+    global time_speed
+    global Buttons
+    global screen
+
+    print('Modelling started!')
+    physical_time = 0
+
+    screen = University("Arial-16", None)
+
+    Buttons = screen.button(5, ["Start", "Open file", "Save to file", "Create Orbit", "Static Save "],
+                            [start_execution, open_file_dialog, save_file_dialog, create_orbit, static_save_dialog])
+
+    time_step = tkinter.DoubleVar()
+    time_step.set(1)
+    entries = screen.entry(1, [time_step])
+
+    time_speed = tkinter.DoubleVar()
+    scales = screen.scale(1, [time_speed], ["horizontal"])
+
+    displayed_time = tkinter.StringVar()
+    displayed_time.set(str(physical_time) + " seconds gone")
+    labels = screen.label(1, [displayed_time], [30])
+
+    screen.location(Buttons, ["left", "", " ", "", ""], entries, ['left'], scales, ["left"], labels, ["right"])
+    screen.location(Buttons, ["left", "left", "left", "left", "left"], entries, [''], scales, [""], labels, [""])
+
+    try:
+        screen.loop()
+    except KeyboardInterrupt:
+        pass
+    print('Modelling finished!')
+
+
+if __name__ == "__main__":
+    main()
---
+++ solar_objects.py
@@ -0,0 +1,353 @@
+import math
+import tkinter
+
+from _tkinter import TclError
+
+from tkinter import ttk
+
+
+class SpaceObjects:
+
+    def __init__(self, filename):
+
+        self.filename = filename
+        self.parameters = []
+        self.type = ""
+        self.Datas = []
+
+    def read_parameters(self):
+
+        with open(self.filename) as input_file:
+            for line in input_file:
+
+                if len(line.strip()) == 0 or line[0] == '#':
+                    continue  # пустые строки пропускаем
+                object_type = line.split()[0]
+                if self.type == object_type:
+                    data = line.split()
+                    try:
+                        self.parameters = [data[0], float(data[1]), data[2], float(data[3]),
+                                           float(data[4]), float(data[5]), float(data[6]), float(data[7]), int(data[8]),
+                                           int(data[9])]
+                    except IndexError:
+                        self.parameters = [data[0], float(data[1]), data[2], float(data[3]),
+                                           float(data[4]), float(data[5]), float(data[6]), float(data[7]), int(data[8])]
+                    self.Datas.append(self.parameters)
+        return (self.Datas)
+
+    def append(self, objects):
+        objects.extend(self.Datas)
+        return (objects)
+
+    def write_data_to_file(self, output_filename, t):
+        with open(output_filename, t) as out_file:
+            for obj in self.Datas:
+                if obj[0] == self.type:
+                    s = ' '.join(map(str, obj))
+                    out_file.write(s + "\n")
+
+    def write_static_to_file(self, output_filename, t):
+        dynamic = self.Datas
+        static = self.read_parameters()
+        with open(output_filename, t) as out_file:
+            for j in range(len(dynamic)):
+                if dynamic[j][0] == self.type:
+                    s = ''
+                    for i in range(len(dynamic[j])):
+                        try:
+                            if float(dynamic[j][i]) - float(static[j][i]) == 0:
+                                s += ' ' + str(static[j][i])
+                        except ValueError:
+                            s += " " + str(static[j][i])
+                    out_file.write(s + "\n")
+
+
+class Star(SpaceObjects):
+    """Тип данных, описывающий звезду.
+    Содержит массу, координаты, скорость звезды,
+    а также визуальный радиус звезды в пикселах и её цвет.
+    """
+
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.type = 'Star'
+
+
+class Planet(SpaceObjects):
+    """Тип данных, описывающий планету.
+        Содержит массу, координаты, скорость планеты,
+        а также визуальный радиус планеты в пикселах и её цвет
+        """
+
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.type = 'Planet'
+
+    def orbit(self):
+        p = Star(self.filename).read_parameters()
+        orbits = []
+        for i in range(len(p)):
+            for j in range(len(self.Datas)):
+                if p[i - 1][8] == self.Datas[j - 1][8]:
+                    r = ((p[i - 1][4] - self.Datas[j - 1][4]) ** 2 + (p[i - 1][5] - self.Datas[j - 1][5]) ** 2) ** 0.5
+                    orbit_cords = [float(p[i - 1][4] - r), float(p[i - 1][5] - r), float(p[i - 1][4] + r),
+                                   float(p[i - 1][5] + r)]
+                    orbits.append(orbit_cords)
+        return (orbits)
+
+    def move(self, objects, dt):
+        for obj in objects:
+            if obj[0] == 'Star':
+                x = obj[4]
+                y = obj[5]
+                ids = obj[8]
+                for body in self.Datas:
+                    if body[8] == ids:
+                        r = ((body[4] - x) ** 2 + (body[5] - y) ** 2) ** 0.5
+                        body[7] += body[3] * dt
+                        body[4] = x + r * math.cos(math.radians(body[7]))
+                        body[5] = y + r * math.sin(math.radians(body[7]))
+
+
+class Sputnik(SpaceObjects):
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.type = 'Sputnik'
+
+    def radius(self, objects):
+        rad = []
+        r = 0
+        for obj in objects:
+            if obj[0] == 'Planet':
+                x = obj[4]
+                y = obj[5]
+                ids = obj[9]
+                for body in self.Datas:
+                    if body[8] == ids:
+                        r = ((body[4] - x) ** 2 + (body[5] - y) ** 2) ** 0.5
+                    rad.insert(ids, r)
+        return (rad)
+
+    def move(self, objects, dt, r):
+        for obj in objects:
+            if obj[0] == 'Planet':
+                x = obj[4]
+                y = obj[5]
+                ids = obj[9]
+                for body in self.Datas:
+                    if body[8] == ids:
+                        body[7] += body[3] * dt
+                        body[4] = x + r[ids] * math.cos(math.radians(body[7]))
+                        body[5] = y + r[ids] * math.sin(math.radians(body[7]))
+
+    def orbit(self):
+        p = Planet(self.filename).read_parameters()
+        orbits = []
+        for i in range(len(p)):
+            for j in range(len(self.Datas)):
+                if p[i - 1][9] == self.Datas[j - 1][8]:
+                    r = ((p[i - 1][4] - self.Datas[j - 1][4]) ** 2 + (
+                            p[i - 1][5] - self.Datas[j - 1][5]) ** 2) ** 0.5
+                    orbit_cords = [float(p[i - 1][4] - r), float(p[i - 1][5] - r), float(p[i - 1][4] + r),
+                                   float(p[i - 1][5] + r)]
+                    orbits.append(orbit_cords)
+
+        return (orbits)
+
+
+class University:
+
+    def __init__(self, font, scale):
+        self.root = tkinter.Tk()
+        self.header_font = font
+        self.scale_factor = scale
+        self.buttons = []
+        self.max_distance = 0
+        self.window_width = self.root.winfo_screenwidth()
+        self.window_height = self.root.winfo_screenheight()
+        self.space = tkinter.Canvas(self.root, width=self.window_width, height=self.window_height - 119, bg="black")
+        self.frame = tkinter.Frame(self.root)
+        self.scale_factor = 0
+        self.t = self.space.create_text(0, 0, font=self.header_font, anchor='nw', text='', fill="white")
+
+    def change_distance(self, max_distance):
+        self.max_distance = max_distance
+        self.scale_factor = min((self.window_height - 119), self.window_width) / (2 * self.max_distance)
+        print(self.window_height, self.window_width, self.scale_factor)
+
+    def location(self, buttons, place_b, entries, place_e, scales, place_s, labels, place_l):
+        self.space.pack(side=tkinter.TOP)
+        self.frame.pack(side=tkinter.BOTTOM)
+        i = 0
+        for button in buttons:
+            try:
+                button.pack(side=place_b[i])
+            except TclError:
+                pass
+            i += 1
+        i = 0
+        for entry in entries:
+            try:
+                entry.pack(side=place_e[i])
+            except TclError:
+                pass
+            i += 1
+        i = 0
+        for scale in scales:
+            try:
+                scale.pack(side=place_s[i])
+            except TclError:
+                pass
+            i += 1
+        i = 0
+        for label in labels:
+            try:
+                label.pack(side=place_l[i])
+            except TclError:
+                pass
+            i += 1
+
+    def button(self, n, text, commands):
+        buttons = []
+        for i in range(n):
+            button1 = tkinter.Button(self.frame, text=text[i], command=commands[i])
+            buttons.append(button1)
+        self.buttons = buttons
+        return (buttons)
+
+    def entry(self, n, text):
+        entries = []
+        for i in range(n):
+            entry = tkinter.Entry(self.frame, textvariable=text[i])
+            entries.append(entry)
+        return (entries)
+
+    def scale(self, n, text, orient):
+        scales = []
+        for i in range(n):
+            scale = tkinter.Scale(self.frame, variable=text[i], orient=orient[i])
+            scales.append(scale)
+        return (scales)
+
+    def label(self, n, text, width):
+        labels = []
+        for i in range(n):
+            one_label = tkinter.Label(self.frame, textvariable=text[i], width=width[i])
+            labels.append(one_label)
+        return (labels)
+
+    def trigger_button(self, i, text, command):
+        self.buttons[i - 1]["text"] = text
+        self.buttons[i - 1]["command"] = command
+
+    def create_image(self, object):
+        """Создаёт отображаемый объект спутника.
+
+        Параметры:
+
+        **space** — холст для рисования.
+        **star** — объект звезды.
+        """
+
+        x = self.scale_x(object[4])
+        y = self.scale_y(object[5])
+        r = object[1]
+        object[6] = self.space.create_oval(x - r, y - r, x + r, y + r, fill=object[2])
+
+    def create_orbit_image(self, objects):
+        orbits = []
+        for obj in objects:
+            for body in objects:
+                if obj[0] == 'Star' and body[0] == 'Planet' and obj[8] == body[8]:
+                    r = ((obj[4] - body[4]) ** 2 + (obj[5] - body[5]) ** 2) ** 0.5
+                    orbit_cords = [float(obj[4] - r), float(obj[5] - r), float(obj[4] + r),
+                                   float(obj[5] + r)]
+                    orbits.append(orbit_cords)
+                if obj[0] == 'Planet' and body[0] == 'Sputnik' and obj[9] == body[8]:
+                    r = ((obj[4] - body[4]) ** 2 + (obj[5] - body[5]) ** 2) ** 0.5
+                    orbit_cords = [float(obj[4] - r), float(obj[5] - r), float(obj[4] + r),
+                                   float(obj[5] + r)]
+                    orbits.append(orbit_cords)
+        for orbit in orbits:
+            self.space.create_oval(self.scale_x(orbit[0]), self.scale_y(orbit[1]), self.scale_x(orbit[2]),
+                                   self.scale_y(orbit[3]),
+                                   outline="white", tags='circle')
+
+    def update_system_name(self, system_name):
+        """Создаёт на холсте текст с названием системы небесных тел.
+        Если текст уже был, обновляет его содержание.
+
+        Параметры:
+
+        **space** — холст для рисования.
+        **system_name** — название системы тел.
+        """
+        self.space.delete(self.t)
+        self.t=self.space.create_text(0, 0,font=self.header_font,anchor='nw', text=system_name, fill="white")
+
+
+
+    def update_object_position(self, objects, orbit_perform):
+        """Перемещает отображаемый объект на холсте.
+
+        Параметры:
+
+        **space** — холст для рисования.
+        **body** — тело, которое нужно переместить.
+        """
+        circles = self.space.find_withtag('circle')
+        if orbit_perform:
+            for circle in circles:
+                self.space.delete(circle)
+            self.create_orbit_image(objects)
+            self.space.tag_lower('circle')
+        if not orbit_perform and circles:
+            for circle in circles:
+                self.space.delete(circle)
+
+        for body in objects:
+            x = self.scale_x(body[4])
+            y = self.scale_y(body[5])
+            r = body[1]
+            if x + r < 0 or x - r > self.window_width or y + r < 0 or y - r > self.window_height:
+                self.space.coords(body[6], self.window_width + r, self.window_height + r,
+                                  self.window_width + 2 * r, self.window_height + 2 * r)  # положить за пределы окна
+            self.space.coords(body[6], x - r, y - r, x + r, y + r)
+
+    def loop(self):
+        self.root.mainloop()
+
+    def after(self, time_speed, command):
+        self.space.after(101 - int(time_speed), command)
+
+    def delete(self, obj):
+        self.space.delete(obj)
+
+    def tag_lower(self, obj):
+        self.space.tag_lower(obj)
+
+    def scale_x(self, x):
+        """Возвращает экранную **x** координату по **x** координате модели.
+        Принимает вещественное число, возвращает целое число.
+        В случае выхода **x** координаты за пределы экрана возвращает
+        координату, лежащую за пределами холста.
+
+        Параметры:
+
+        **x** — x-координата модели.
+        """
+        return int(x * self.scale_factor) + self.window_width / 8
+
+    def scale_y(self, y):
+        """Возвращает экранную **y** координату по **y** координате модели.
+        Принимает вещественное число, возвращает целое число.
+        В случае выхода **y** координаты за пределы экрана возвращает
+        координату, лежащую за пределами холста.
+        Направление оси развёрнуто, чтобы у модели ось **y** смотрела вверх.
+
+        Параметры:
+
+        **y** — y-координата модели.
+        """
+
+        return int(y * self.scale_factor) + (self.window_height - 119) / 10
